2ccdf242c125529a5b5030046bdca325adc60145
fausto-mateus <maafau@gmail.com>
2021-06-23 15:20:26 -0300

    subindo repositório

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StockCardComponent } from './stockcard/stock-card.component';
+
+@NgModule({
+  declarations: [
+    DashboardPageComponent,
+    StockCardComponent,
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+  ],
+  exports:[
+    DashboardPageComponent
+  ]
+})
+export class DashboardModule { }
